f23239209e7123931f2cdb955bbdbe25508a36fb
samorae101@gmail.com <samorae101@gmail.com>
2015-02-22 18:06:33 +0000

    customized my app

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 
 This file creates your application.
 """
-
+import smtplib
 from app import app
 from flask import render_template, request, redirect, url_for
 
@@ -29,6 +29,74 @@ def about():
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+@app.route("/sendemail", methods=['GET', 'POST'])
+def sendemail():
+  if request.method == 'POST':
+    name = request.form['name']
+    email = request.form['email']
+    subject = str(request.form['subject'])
+    user_message = request.form['message']
+    fromaddr = email
+    toaddr = 'samorae101@gmail.com'
+
+    #message = {"fromname": fromname, "fromaddr": fromaddr}
+
+    fromname= str(name)
+    toname = "Samorae"
+    #subject = 
+    msg = str(user_message)
+     #msg2 = "Hi"
+
+
+
+    message = """From:  %r  <%r>
+
+    To: %r <%r> 
+
+    Subject: %r
+
+
+    %r
+
+
+    """ % (fromname, fromaddr, toname, toaddr, subject, msg)
+
+    messagetosend = message.format(
+
+    fromname,
+
+    fromaddr,
+
+    toname,
+
+    toaddr,
+
+    subject,
+
+    msg)
+
+    # Credentials (if needed)
+
+    username = 'samorae101@gmail.com'
+
+    password = 'sunshinemkc'
+
+    # The actual mail send
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+
+    server.starttls()
+
+    server.login(username,password)
+
+    server.sendmail(fromaddr, toaddr, messagetosend)
+
+    print "Message sent - %s, %s, %s, %s" % (name, email, subject, user_message)
+
+    server.quit()
+    return "<div> <h1> Your email has been sent <i>%s</i>, Thank you </h1></div>" % name
+  return "Hi"
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
